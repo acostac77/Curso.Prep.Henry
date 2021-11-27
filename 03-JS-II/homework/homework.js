974aedc918e520132d0733b0bd0201987b2ce7cc
acostac77 <ariel08_acost@hotmail.com>
2021-11-27 19:53:10 -0300

    ejercicio 3 y 8 y 9

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,14 +5,29 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if (x > y){
+    return x;
+  } 
+  if (y > x){
+      return y;    
+  }
+      return x;
 }
+
+
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  if( edad >= 18){
+    return "Allowed";
+} 
+else {
+  return "Not allowed";
 }
-  
+}
+
 function conection(status) {
   //Recibimos un estado de conexión de un usuario representado por un valor numérico. 
   //Cuando el estado es igual a 1, el usuario está "Online"
@@ -20,6 +35,15 @@ function conection(status) {
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
 }
+if(status === 1){
+  return "Online";
+} else if( status === 2){
+  return "Away"
+} else {
+  return "Offline";
+}
+
+  
 
 function saludo(idioma) {
   // Devuelve un saludo en tres diferentes lenguajes:
@@ -28,7 +52,20 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
-}
+  if (idioma === "aleman") {
+    return "Guten Tag!";
+  }
+    if (idioma === "mandarin") {
+      return "Ni Hao!";
+     }
+     if (idioma === "ingles"){
+       return "Hello!";
+     }
+         return "hola!"
+       }
+      
+    
+
 
 function colors(color) {
   //La función recibe un color. Devolver el string correspondiente:
@@ -38,6 +75,19 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+  switch(color){
+    case "blue": 
+    return "This is blue";
+    case "red": 
+      return "This is red";
+      case "green":
+      return "This is green";
+      case "orange":
+        return "This is orange";
+
+        default: "Color not found";
+
+  }
 }
 
 function esDiezOCinco(numero) {
